073151c9254fd9e1364367c97aa6c19f373ad5c6
Miguelggit <contato.miguel222@gmail.com>
2025-07-31 21:48:29 -0300

    Criando logica de recursividade e uso de break

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,62 @@
 #include <stdio.h> 
+
+void recursivoRainha(int contagem){
+        char direcao[10];
+        char pecaEscolhida[10] = "Rainha";
+        int casas;
+        printf("A %s anda em qualquer direção. Escolha a direcao: ");
+        scanf(" %s", direcao);
+        printf("Quantas casas ela vai andar: ");
+        scanf(" %d", &casas);
+        printf("A %s vai se mover para a %s, andando %d casas\n", pecaEscolhida, direcao, casas);
+    if(contagem <= casas){
+        printf("Movendo a rainha para a %s %d°\n", direcao, contagem);
+        recursivoRainha(contagem + 1);
+    }
+
+}
+
+void recursivoTorre(int contagem){
+        char direcao2[10];
+        char pecaEscolhida2[10] = "Torre";
+        int casas2;
+        char posicao2[20];
+        printf("A %s Move-se em linha reta horizontalmente ou verticalmente. Escolha a direcao: ");
+        scanf(" %s", direcao2);
+
+        printf("Escolha o sentido: ");
+        scanf(" %s", posicao2);
+
+        printf("Quantas casas ela vai andar: ");
+        scanf(" %d", &casas2);
+        
+        printf("A %s vai se mover na %s para a %s, andando %d casas\n", pecaEscolhida2, direcao2, posicao2, casas2);
+
+        if(contagem <= casas2) {
+        printf("Movendo a %s na %s para a %s %d°\n",pecaEscolhida2, direcao2, posicao2, contagem);
+        recursivoTorre(contagem + 1);
+    }
+
+}
+
+void recursivoBispo(int contagem){
+        char direcao3[10];
+        char pecaEscolhida3[10] = "Bispo";
+        int casas3;
+        char posicao3[20];
+        printf("A %s  Move-se na diagonal. Escolha a direcao(CIMA | BAIXO): ");
+        scanf(" %s", direcao3);
+        printf("Escolha o sentido(DIREITA | ESQUERDA): ");
+        scanf(" %s", posicao3);
+        printf("Quantas casas ela vai andar: ");
+        scanf(" %d", &casas3);
+        printf("A %s vai se mover na %s para a %s, andando %d casas\n", pecaEscolhida3, direcao3, posicao3, casas3);
+
+        if(contagem <= casas3){
+            printf("Movendo o %s para %s e para a %s %d°\n",pecaEscolhida3, direcao3, posicao3, contagem);
+            recursivoBispo(contagem + 1);
+        }
+}
 int main() {
     int escolhaPeca;
     printf("Escolha uma peça para jogar\n");
@@ -11,63 +69,23 @@ int main() {
     switch (escolhaPeca)
     {
     case 1:
-        char direcao[10];
-        char pecaEscolhida[10] = "Rainha";
-        int casas;
-        int contagem = 1;
-        printf("A %s anda em qualquer direção. Escolha a direcao: ");
-        scanf(" %s", direcao);
-        printf("Quantas casas ela vai andar: ");
-        scanf(" %d", &casas);
-        printf("A %s vai se mover para a %s, andando %d casas\n", pecaEscolhida, direcao, casas);
-        while (contagem <= casas)
-        {
-            printf("Movendo a rainha para a %s %d°\n", direcao, contagem);
-            contagem++;
-        }
+        int quantidade = 1;
+            recursivoRainha(quantidade);
+            break;
 
-        break;
-
-        case 2: 
-        char direcao2[10];
-        char pecaEscolhida2[10] = "Torre";
-        int casas2;
-        char posicao2[20];
-        printf("A %s Move-se em linha reta horizontalmente ou verticalmente. Escolha a direcao: ");
-        scanf(" %s", direcao2);
-
-        printf("Escolha o sentido: ");
-        scanf(" %s", posicao2);
-        printf("Quantas casas ela vai andar: ");
-        scanf(" %d", &casas2);
-        printf("A %s vai se mover na %s para a %s, andando %d casas\n", pecaEscolhida2, direcao2, posicao2, casas2);
-        for (size_t contagem = 1; contagem <= casas2; contagem++)
-        {
-            printf("Movendo a %s na %s para a %s %d°\n",pecaEscolhida2, direcao2, posicao2, contagem);
-        }
+    case 2: 
+        int quantidade2 = 1;
+        recursivoTorre(quantidade2);
         
-        break;
+            break;
     
-        case 3:
-        char direcao3[10];
-        char pecaEscolhida3[10] = "Bispo";
-        int casas3;
-        char posicao3[20];
-        int contagem3 = 1;
-        printf("A %s  Move-se na diagonal. Escolha a direcao(CIMA | BAIXO): ");
-        scanf(" %s", direcao3);
-        printf("Escolha o sentido(DIREITA | ESQUERDA): ");
-        scanf(" %s", posicao3);
-        printf("Quantas casas ela vai andar: ");
-        scanf(" %d", &casas3);
-        printf("A %s vai se mover na %s para a %s, andando %d casas\n", pecaEscolhida3, direcao3, posicao3, casas3);
-        do{
-        printf("Movendo o %s para %s e para a %s %d°\n",pecaEscolhida3, direcao3, posicao3, contagem3);
-        contagem3++;
-        }while( contagem3 <= casas3);
+    case 3:
+        int quantidade3 = 1;
+        recursivoBispo(quantidade3);
             break;
 
         case 4: 
+
             int c = 1;
             char sentidoCavalo[10];
             char direcaoCavalo[10];
@@ -81,16 +99,20 @@ int main() {
 
             printf("No final do L ele vai para um dos dois lados, esquerda ou direita. Escolha o lado: ");
             scanf(" %s", ladoCavalo);
-
+        if(sentidoCavalo != "horizontal" || sentidoCavalo != "vertical" || direcaoCavalo != "CIMA" || direcaoCavalo != "BAIXO" || direcaoCavalo != "LADO DIREITO" || direcaoCavalo != "LADO ESQUERDO" || ladoCavalo != "direita" || ladoCavalo != "esquerda"){
+            printf("Algo deu errado!");
+            break;
+        } else{
+         
             for(size_t i = 0; i < contagemCavalo; i++){
                 do{
                     printf(" %s para %s - %d°\n", sentidoCavalo, direcaoCavalo, c);
                     c++;
                 }while(c <= 2);
                 printf(" vai ficar na casa da %s", ladoCavalo);
-            }
+            }   
+        }
 
-            
             break;
 
     default:
